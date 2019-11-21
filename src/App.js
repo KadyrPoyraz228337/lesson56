5d0923e86b1b012c9f6988af0eceb43208b4f6c1
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-11-21 15:53:57 +0200

    Создали state App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import "./App.css";
 
 class App extends Component {
+    state = {
+        items: []
+    };
+
     render() {
         return (
             <div>
-              
+
             </div>
         );
     }
